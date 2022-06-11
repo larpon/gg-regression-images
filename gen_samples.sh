e3609cad82a0883155d8ad1404b66ee13b517076
Delyan Angelov <delian66@gmail.com>
2022-06-11 11:41:10 +0300

    ensure stable order in gen_samples.sh

--- gen_samples.sh
+++ gen_samples.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-montage -verbose -label '%f' -font Helvetica -pointsize 10 -background '#000000' -fill 'gray' -define jpeg:size=200x200 -geometry 200x200+2+2 -auto-orient $(find examples -type f) ./samples.jpg
+montage -verbose -label '%f' -font Helvetica -pointsize 10 -background '#000000' -fill 'gray' -define jpeg:size=200x200 -geometry 200x200+2+2 -auto-orient $(find examples -type f|sort) ./samples.jpg
